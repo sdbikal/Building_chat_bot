5cde92ac454686325c7569f849c7aee6c33618a7
isakovsh <isakovsh19@gmail.com>
2024-01-25 20:28:46 +0500

    uploaded  demo

---
+++ main.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import streamlit as st
+from agent import qa_dev
+
+st.title("MOJ demo")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if question := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": question})
+    with st.chat_message("user"):
+        st.markdown(question)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = qa_dev(question)
+        
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
