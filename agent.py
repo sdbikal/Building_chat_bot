abd7ea7e0667444642f177cf6715888c92ecfafb
isakovsh <isakovsh19@gmail.com>
2024-01-31 09:10:56 +0500

    uploaded src

---
+++ agent.py
@@ -0,0 +1,34 @@
+import os
+import qdrant_client
+import openai
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import Qdrant
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+client = qdrant_client.QdrantClient(
+    os.getenv("QDRANT_HOST"),
+    api_key = os.getenv("QDRANT_API_KEY")
+)
+
+embeddings = OpenAIEmbeddings()
+
+vectorstore = Qdrant(
+        client=client,
+        collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
+        embeddings=embeddings
+    )
+
+def qa_dev(user_query:str):
+    llm = ChatOpenAI(model='gpt-4-1106-preview')
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore.as_retriever()
+        )
+    result = qa.invoke({'query':user_query})
+    return result['result']
---
+++ test.py
@@ -0,0 +1,2 @@
+from agent import qa_dev
+print(qa_dev("hi"))
